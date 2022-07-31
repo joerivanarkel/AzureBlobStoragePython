0f849af912c21ea999ef3304e25ba6a0d6bd1f1a
Joeri van Arkel <98022003+joerivanarkel@users.noreply.github.com>
2022-07-31 22:10:31 +0200

    outcommented

--- project/BlobModule_test.py
+++ project/BlobModule_test.py
@@ -8,27 +8,6 @@ from dotenv import dotenv_values
 import os, uuid
 from azure.storage.blob import BlobServiceClient, __version__
 
-# # Connection string
-# config = dotenv_values(".env")
-# connectionstring = config["CONNECTIONSTRING"]
-# # make file
-# local_path = "./project/data"
-# local_file_name = str(uuid.uuid4()) + ".txt"
-# upload_file_path = os.path.join(local_path, local_file_name)
-# download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
-    
-# # write to file
-# file = open(upload_file_path, 'w')
-# file.write("Hello, World!")
-# file.close()  
-
- 
-# # create container
-# blob_service_client = BlobServiceClient.from_connection_string(connectionstring)
-# container_name = str(uuid.uuid4())
-# container_client = blob_service_client.create_container(container_name)
-# blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
 def test_upload_blob():
     # Connection string
     config = dotenv_values(".env")
